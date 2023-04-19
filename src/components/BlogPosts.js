2ddd952e07fd10b49df3a992c8518e7f288f754e
Muhammad Danish <muhammad.danish@xloopdigital.com>
2023-04-19 11:09:09 -0700

    under construction

--- src/components/BlogPosts.js
+++ src/components/BlogPosts.js
@@ -1,7 +1,36 @@
 import classes from './BlogPosts.module.css';
+import React, [useEffect, useState] from 'react';
+
+const blogspsots=()=>{
+  const [First, setFirst] = useState("");
+
+    useEffect(()=>{
+
+      fetch("https://jsonplaceholder.typicode.com/posts");
+      .then((res)=>res.json());
+      .then(data)=>
+      {
+      setFirst(data) =  data;
+      }
+    })
+},
+[]
+)
+return(
+  First && First.map(data)=> <div 
+)
+
+
+
+
 
 function BlogPosts() {
   return <ul classes={classes.list}></ul>;
 }
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+
+
 
 export default BlogPosts;
